97b571e3fa2f7503850ed81ef25f96c000eb7bb9
meghasharma123 <meghasharma2822000@gmail.com>
2026-06-14 18:32:53 +0530

    added user to database with code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const { adminAuth, userAuth } = require('./middlewares/AuthenticationCheck');
 const { connectDatabase } = require('./config/database');
+const { User } = require('./models/user')
 
 const app = express();
+
+app.post("/user", async (req, res) => {
+    const user1 = new User({
+        firstName: "megha",
+        lastName: "sharma",
+        email: "megha@sharma.com",
+        password: "megha@12232"
+    });
+
+    try {
+        await user1.save();
+        res.send("User saved successfully!");
+    } catch (error) {
+        res.status(500).send("Error saving data: ", error.message);
+    }
+})
 
 connectDatabase().then(() => {
     console.log("Connection established.");
