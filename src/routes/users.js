98678d6e3fd36ec6e17ae9abe7eda1bd9c071837
meghasharma123 <meghasharma2822000@gmail.com>
2026-06-27 17:08:50 +0530

    connections list

--- src/routes/users.js
+++ src/routes/users.js
@@ -21,4 +21,33 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
     }
 })
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const data = await ConnectionRequestModel
+            .find({
+                $or: [
+                    { fromUserId: loggedInUser._id, status: "accepted" },
+                    { toUserId: loggedInUser._id, status: "accepted" }
+                ]
+            })
+            .populate("fromUserId", ["firstName", "emailId"])
+            .populate("toUserId", ["firstName", "emailId"]);
+
+
+        const listOfConnections = data.map((ele) => {
+            if (loggedInUser._id.equals(ele.fromUserId._id)) {
+                return ele.toUserId;
+            }
+            return ele.fromUserId;
+        });
+
+        res.json({ "meesgae": "Fetched", "body": listOfConnections });
+
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
 module.exports = { userRouter };
